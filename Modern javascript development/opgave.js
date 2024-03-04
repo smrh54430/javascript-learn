0d169f5784f02cafa22a7604e75e1cba6f912d18
Melledodo <132983337+Melledodo@users.noreply.github.com>
2024-03-04 20:05:34 +0100

    Add files via upload

---
+++ Modern javascript development/opgave.js
@@ -0,0 +1,32 @@
+// console.log('Name: ' + name + ' - Datatype: ' + typeof(name)); //John Doe
+// console.log('Age: ' + age + ' - Datatype: ' + typeof(age)); //30
+// console.log('Married: ' + isMarried + ' - Datatype: ' + typeof(isMarried)); //false
+// console.log('Salary: ' + salary + ' - Datatype: ' + typeof(salary)); //12345678901234567890n
+// console.log('ID: ');
+// console.log(id); //symbol(unique_identifier)
+// console.log('ID Datatype: ' + typeof(id)); //symbol(unique_identifier)
+// console.log('Object: ');
+
+
+// opgave 1.2
+function checkUserInput() {
+let username = document.getElementById('username').value;
+let password = document.getElementById('password').value;;
+
+    if (username.trim().length > 0 && password.trim().length > 0) {
+        alert('Velkommen ' + username); }
+    else {
+        alert('Indtast venligst et gyldigt brugernavn og kodeord.');
+    }
+
+    document.getElementById("username").value = "";
+    document.getElementById("password").value = "";
+}
+
+// opgave 1.3
+function convertCurrency() {
+    let usd = 55.00;
+    let danskePenge = new Intl.NumberFormat('da-DK', {style: 'currency', currency: 'DKK'});
+    console.log(danskePenge.format(usd*7));
+}
+convertCurrency();
