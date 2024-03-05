87db1dcecaa96faafa1c378237bc2f56b977a85b
Melledodo <mkaagaard@hotmail.com>
2024-03-05 13:10:09 +0100

    Update opgave.js

--- Modern javascript development/opgave.js
+++ Modern javascript development/opgave.js
@@ -10,23 +10,37 @@
 
 // opgave 1.2
 function checkUserInput() {
-let username = document.getElementById('username').value;
-let password = document.getElementById('password').value;;
-
-    if (username.trim().length > 0 && password.trim().length > 0) {
-        alert('Velkommen ' + username); }
-    else {
-        alert('Indtast venligst et gyldigt brugernavn og kodeord.');
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    const regexEmail = /\S+@\S+\.\S+/;
+    
+        if (password.length < 8) {
+            alert('WROOOOONG');
+        } else{
+            alert('welcome')
+        }
+    
+        if (regexEmail.test(username)) {
+            alert('YEEEEAAAAAAHHHHHH')
+        } else {
+            alert('BUUUUUUUHHHHHH')
+        }
+    
+        if (username.trim().length > 0 && password.trim().length > 0) {
+            alert('Velkommen ' + username); }
+        else {
+            alert('Indtast venligst et gyldigt brugernavn og kodeord.');
+        }
+    
+        document.getElementById("username").value = "";
+        document.getElementById("password").value = "";
     }
-
-    document.getElementById("username").value = "";
-    document.getElementById("password").value = "";
-}
-
-// opgave 1.3
-function convertCurrency() {
-    let usd = 55.00;
-    let danskePenge = new Intl.NumberFormat('da-DK', {style: 'currency', currency: 'DKK'});
-    console.log(danskePenge.format(usd*7));
-}
-convertCurrency();
+    
+    // opgave 1.3
+    function convertCurrency() {
+        let usd = 55.00;
+        let danskePenge = new Intl.NumberFormat('da-DK', {style: 'currency', currency: 'DKK'});
+        console.log(danskePenge.format(usd*7));
+    }
+    convertCurrency();
+    
