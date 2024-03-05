7b9421c2083016da87558817743cf0850557ee95
Melledodo <mkaagaard@hotmail.com>
2024-03-05 14:02:42 +0100

    Update opgave.js

--- Modern javascript development/opgave.js
+++ Modern javascript development/opgave.js
@@ -44,9 +44,9 @@ function checkUserInput() {
     }
     convertCurrency();
 
-    function generateRandomNumber() {
-        return Math.floor(Math.random() * (500 - 66 + 1)) + 66;
+    function generateRandomNumber(min = 66, max = 500) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    console.log(`Execute Order ${generateRandomNumber()}`);
+    console.log(`Execute Order ${generateRandomNumber(1, 5)}`);
     generateRandomNumber();
     
